b7506257b1ef1ab0a7899e520ca96b162afdbf79
Eric Holk <eric.holk@gmail.com>
2013-11-16 21:32:13 -0500

    Refactoring.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .phony: all
 all : 
 	rustc -O -c --lib boot.rs
-	x86_64-efi-pe-ld -g --oformat pei-x86-64 --dll --subsystem 10:1.0 --major-os-version 0 --heap 0,0 -nostdlib --stack 0x1000,0 -pie -Bsymbolic -nocombreloc -e efi_main boot.o -o boot.efi
+	x86_64-efi-pe-ld -g --oformat pei-x86-64 --dll --subsystem 10:1.0 --major-os-version 0 --heap 0,0 -nostdlib --stack 0x200000,0 -pie -Bsymbolic -nocombreloc -e efi_start boot.o -o boot.efi
 	mkdir -p img/efi/boot
 	cp boot.efi img/efi/boot/bootx64.efi
 	mkisofs -o boot.iso img
--- boot.rs
+++ boot.rs
@@ -1,62 +1,24 @@
 #[no_std];
 
-type EFI_HANDLE = *();
+use uefi::SimpleTextOutput;
 
-struct EFI_TABLE_HEADER {
-    Signature  : u64,
-    Revision   : u32,
-    HeaderSize : u32,
-    CRC32      : u32,
-    priv Reserved : u32
-}
+mod uefi;
 
-struct EFI_SYSTEM_TABLE {
-    Hdr : EFI_TABLE_HEADER,
-    FirmwareVendor : *u16,
-    FirmwareRevision : u32,
-    ConsoleInHandle : EFI_HANDLE,
-    ConIn : *EFI_SIMPLE_TEXT_INPUT_PROTOCOL,
-    ConsoleOutHandle : EFI_HANDLE,
-    ConOut : *EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL,
-    // ... other stuff that we're ignoring for now.
-}
-
-struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
-
-struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-    Reset : EFI_TEXT_RESET,
-    OutputString : EFI_TEXT_STRING,
-    // ... and more stuff that we're ignoring.
-}
-
-type EFI_TEXT_RESET = *();
-
-type EFI_TEXT_STRING = extern "win64" fn(*EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL,
-                                            *u16);
-
-#[no_mangle]
 #[no_split_stack]
-pub extern "win64" fn efi_main(_ImageHandle : EFI_HANDLE, SystemTable : *EFI_SYSTEM_TABLE) -> int {
+pub fn efi_main(sys : uefi::SystemTable) {
+    let hello = "H\0e\0l\0l\0o\0,\0 \0W\0o\0r\0l\0d\0!\0\n\0\r\0";
+
     unsafe {
-        let SystemTable = *SystemTable;
-        let vendor = SystemTable.FirmwareVendor;
-        let conout = SystemTable.ConOut;
-        let output = (*conout).OutputString;
-
-        let hello = "H\0e\0l\0l\0o\0,\0 \0W\0o\0r\0l\0d\0!\0\n\0\r\0";
-
-        output(conout, str_as_u16(hello));
+        sys.console().write(str_as_u16_slice(hello));
 
         loop {
         }
     }
 }
 
-fn str_as_u16(s: &str) -> *u16 {
-    unsafe {
-        let (p, _) : (*u8, uint) = transmute(s);
-        transmute(p)
-    }
+unsafe fn str_as_u16_slice(s: &str) -> &[u16] {
+        let (p, l) : (*u8, uint) = transmute(s);
+        transmute((p, l >> 1))
 }
 
 fn buf_ptr<T>(buf: &[T]) -> (*T, uint) {
@@ -71,5 +33,14 @@ fn buf_ptr<T>(buf: &[T]) -> (*T, uint) {
 
 extern "rust-intrinsic" {
     pub fn transmute<T,U>(val: T) -> U;
-//    pub fn size_of<T>() -> uint;
+}
+
+// Ideally this next function would be in uefi.rs, but the linker
+// forgets about it there.
+#[no_mangle]
+#[no_split_stack]
+pub extern "win64" fn efi_start(_ImageHandle : uefi::EFI_HANDLE,
+                                sys_table : *uefi::EFI_SYSTEM_TABLE) -> int {
+    efi_main(uefi::SystemTable(sys_table));
+    0
 }
